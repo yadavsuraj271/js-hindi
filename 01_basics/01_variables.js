ea8f7599b9c1a7e24e79872adcbc6df88664fa28
suraj yadav <157879813+yadavsuraj271@users.noreply.github.com>
2026-02-20 21:34:20 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,4 +2,22 @@ const accountID = 14450
 let accountEmail = "surajyadv271@gmail.com"
 var accountPassword = "12345"
 accountCity = "gurugram"
+let accountState;
+
+accountEmail ="suraj@gmail.com"
+accountPassword = "123"
+accountCity = "nihalwas"
+// js me bina kuch var k aage kuch   likhe bhi hum  change kr skte hai but dont use  not good
+
+// js me variable or constants ko kuch is type se declare kr skte hai , const ka ek hi trika hai  const use krna , lekin variables ka let or var do hai , js  me ek problem thi ki isko nahi pta tha ki scope kya hai , ye jo jo curly braces hote hai{} inhi ko scope bola jat hai  use kaafi hai jaise if and else me , for me  etc , intially js  scope k basis prr kaam nahi krti thi, earlier block scope naam ki chej pr no control//
+//  ek programmer aaya usne account pswd ki value set krdi phir dusra aaya usne koi or krdi  usne condition likhi for ki usme  account pswd use krlia to glti ye hoti thi account pswd main declared change hojata tha //
+//  ab kisi ne name variable 10 jgh use kia to 10 jgh change hojata tha //
+
+//  modern js me  const or let hi use krte hai var old skool ho chuka   because of  block scope or functional scope//
+
+// accountID = 2
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table ([accountID, accountEmail,accountPassword, accountCity, accountState]);
 
