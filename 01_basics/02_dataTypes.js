9fb7f39fdd9515e3ad597fb55ade14416b66ef73
suraj yadav <157879813+yadavsuraj271@users.noreply.github.com>
2026-02-20 22:08:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all code as newer version
+// alert(3 + 3) //we are using node js not browser version
+
+
+let name = "suraj"
+// string datatype
+let age = 12;
+// number
+let isloggedin = true
+// boolean dataType
+
+// number => 2 to power of 53 
+// bigint no is bht bda specially in trading etc reddit etc
+//  string = " " single or double quote me 
+// boolean =>true or false
+// null =>actually me ye standalone value hai  also datatype but standalone value hai 
+// undefined => when we dont defined value ex accountstate defined but not  used for ex let state = null; actually undefined mtlb  abhi tk value define nhi hui hai and null ek standalone value hai 
+// symbol => mostly used in react , uniquesness find k liye in react  , used in  figma tools etc 
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+
+// jab aap null ka type dekhte hai to aapko object milta hai
+
+console.log(typeof undefined);
+// undefined ka type undefined hi hai kyuki ye ek type hai 
+
