325c6fa57abe067238d1701bc2a08c3d95b87c28
suraj yadav <157879813+yadavsuraj271@users.noreply.github.com>
2026-02-21 09:09:21 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -7,7 +7,7 @@ let accountState;
 accountEmail ="suraj@gmail.com"
 accountPassword = "123"
 accountCity = "nihalwas"
-// js me bina kuch var k aage kuch   likhe bhi hum  change kr skte hai but dont use  not good
+// js me bina kuch var k aage kuch   likhe bhi hum  change kr skte hai but dont use  not good in general
 
 // js me variable or constants ko kuch is type se declare kr skte hai , const ka ek hi trika hai  const use krna , lekin variables ka let or var do hai , js  me ek problem thi ki isko nahi pta tha ki scope kya hai , ye jo jo curly braces hote hai{} inhi ko scope bola jat hai  use kaafi hai jaise if and else me , for me  etc , intially js  scope k basis prr kaam nahi krti thi, earlier block scope naam ki chej pr no control//
 //  ek programmer aaya usne account pswd ki value set krdi phir dusra aaya usne koi or krdi  usne condition likhi for ki usme  account pswd use krlia to glti ye hoti thi account pswd main declared change hojata tha //
